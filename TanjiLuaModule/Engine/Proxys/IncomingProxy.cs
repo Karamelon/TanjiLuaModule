4cab2e1741985e4472c937d8ce2b4fc916d16748
Karamelon <karamelon1@gmail.com>
2018-05-19 15:00:11 -0300

    Code rewrite 1.0.3

---
+++ TanjiLuaModule/Engine/Proxys/IncomingProxy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MoonSharp.Interpreter;
+using TanjiLuaModule.Engine.Types;
+
+namespace TanjiLuaModule.Engine.Proxys
+{
+    internal class IncomingProxy
+    {
+        private readonly IncomingType _type;
+        
+        [MoonSharpHidden]
+        public IncomingProxy(IncomingType type)
+        {
+            this._type = type;
+        }
+
+        public void Send(DynValue value, List<DynValue> values){
+            _type.Send((ushort) value.Number, values);
+        }
+
+        public void Register(DynValue value)
+        {
+            if (value.Type == DataType.Number)
+            {
+                _type.Register((int) value.Number);
+            }
+        }
+    }
+}
